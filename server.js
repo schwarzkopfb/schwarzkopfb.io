e666e22336b9d666f02bd081e1273cce3a08de91
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2018-11-22 17:42:07 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const { resolve } = require('path'),
+      express = require('express'),
+      app = express()
+
+function serve(res, ...paths) {
+    res.sendFile(resolve(__dirname, 'public', ...paths))
+}
+
+app.use(express.static(resolve(__dirname, 'public')))
+
+//app.get('/:page', (req, res) => serve(res, 'index.html'))
+
+app.get('/content/:page', (req, res) => serve(res, 'assets', 'content', req.params.page + '.html'))
+
+app.all('*', (req, res) => res.redirect('/'))
+
+app.listen(3000, () => console.log('server is ready to accept connections on port 3000'))
