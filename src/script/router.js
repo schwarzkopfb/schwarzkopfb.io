4bf1c787e3be40c8fa333a83252f948014d93bbf
Balazs Schwarzkopf <schwarzkopfb@icloud.com>
2019-04-21 18:05:21 +0200

    refactor router

--- src/script/router.js
+++ src/script/router.js
@@ -1,65 +1,67 @@
+import Vue from 'vue'
+
 import navLink from '../views/nav-link'
 
-export default {
-    install(Vue) {
-        let subscribed
+const router = {
+    data() {
+        return {
+            page: {},
+            pages: []
+        }
+    },
 
-        Vue.component('nav-link', navLink)
+    methods: {
+        init(pages) {
+            pages.forEach(p =>
+                p.component = notifyComponentLoaded(p.component, () =>
+                    this.$emit('page:mounted')))
 
-        Vue.prototype.$router = new Vue({
-            data: {
-                page: {},
-                pages: []
-            },
+            this.pages = pages
 
-            methods: {
-                init(pages) {
-                    pages.forEach(p =>
-                        p.component = notifyComponentLoaded(p.component, () =>
-                            this.$emit('page:mounted')))
+            // subscribe for url changes
+            window.addEventListener('popstate', () => this.navigate())
 
-                    this.pages = pages
+            // go to the page (if) specified by the url
+            this.navigate()
+        },
 
-                    // subscribe for url changes (only once)
-                    if (!subscribed) {
-                        subscribed = true
-                        window.addEventListener('popstate', () => this.navigate())
-                    }
+        navigate(page) {
+            if (!page)
+                page = this.getMenuItemByUrl()
+            else
+                page = this.getMenuItemByLink(page)
 
-                    // go to the page (if) specified by the url
-                    this.navigate()
-                },
+            this.page = page
 
-                navigate(page) {
-                    if (!page)
-                        page = this.getMenuItemByUrl()
-                    else
-                        page = this.getMenuItemByLink(page)
+            return page.link
+        },
 
-                    this.page = page
+        getMenuItemByLink(link) {
+            return this.pages.find(item => item.link === link)
+        },
 
-                    return page.link
-                },
-
-                getMenuItemByLink(link) {
-                    return this.pages.find(item => item.link === link)
-                },
-
-                getMenuItemByUrl() {
-                    // pass back the first page by default
-                    return this.getMenuItemByLink(location.pathname) || this.pages[ 0 ]
-                }
-            }
-        })
-
-        function notifyComponentLoaded(getComp, fn) {
-            return () => {
-                return getComp().then(comp => {
-                    comp = comp.default // es6 module
-                    comp.mounted = fn
-                    return comp
-                })
-            }
+        getMenuItemByUrl() {
+            // pass back the first page by default
+            return this.getMenuItemByLink(location.pathname) || this.pages[ 0 ]
         }
     }
 }
+
+function notifyComponentLoaded(getComp, fn) {
+    return () => {
+        return getComp().then(comp => {
+            comp = comp.default // es6 module
+            comp.mounted = fn
+            return comp
+        })
+    }
+}
+
+const Router = Vue.extend({ mixins: [ router ] })
+
+function install(Vue) {
+    Vue.component('nav-link', navLink)
+    Vue.prototype.$router = new Router
+}
+
+export default { install, Router }
