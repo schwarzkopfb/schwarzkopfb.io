4bdf736004adb90880f3cb30f12f02661de6d4f2
Balazs Schwarzkopf <schwarzkopfb@icloud.com>
2019-10-17 01:25:28 +0200

    simplify server

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,15 +1,16 @@
 'use strict'
 
-const { resolve } = require('path'),
+const { resolve, basename } = require('path'),
       { readFileSync } = require('fs'),
       { pages } = require('yaml').parse(readFileSync(pathTo('content', 'site.yml'), 'utf8')),
+      CopyPlugin = require('copy-webpack-plugin'),
       VueLoaderPlugin = require('vue-loader/lib/plugin'),
       HtmlWebpackPlugin = require('html-webpack-plugin'),
       PrerenderSPAPlugin = require('prerender-spa-plugin'),
       Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
-// this file is under 'config' folder
 function pathTo(...relativePaths) {
+    // '..' is needed because this file is under the 'config' folder
     return resolve(__dirname, '..', ...relativePaths)
 }
 
@@ -59,6 +60,9 @@ module.exports = () => ({
     },
     plugins: [
         new VueLoaderPlugin,
+        new CopyPlugin([
+            { from: pathTo('static'), to: pathTo('dist') }
+        ]),
         new HtmlWebpackPlugin({
             hash: true,
             filename: pathTo('dist', 'index.html'),
@@ -67,10 +71,25 @@ module.exports = () => ({
         new PrerenderSPAPlugin({
             staticDir: pathTo('dist'),
             routes: Object.values(pages).map(p => p.link),
+
+            // property injection is needed to be able to detect pre-render stage in client-side code
             renderer: new Renderer({
                 injectProperty: 'renderContext',
                 inject: {}
-            })
+            }),
+
+            // overwrite output paths to make them more suitable for `serve-handler`
+            // (it requires a `404.html` and this behaviour cannot be overridden)
+            // for example: /dist/about/index.html -> /dist/about.html
+            postProcess (renderedRoute) {
+                const fileName = renderedRoute.route === '/'
+                    ? 'index.html'
+                    : basename(renderedRoute.route) + '.html'
+
+                renderedRoute.outputPath = pathTo('dist', fileName)
+        
+                return renderedRoute
+              }
         })
     ],
     resolve: {
--- server.js
+++ server.js
@@ -1,40 +1,15 @@
 'use strict'
 
 const { resolve } = require('path'),
-      fs = require('fs'),
-      express = require('express'),
-      yaml = require('yaml'),
-      app = express(),
-      port = process.env.PORT || 3000,
-      { pages } = yaml.parse(fs.readFileSync(resolve(__dirname, 'content', 'site.yml'), 'utf8'))
+      { createServer } = require('http'),
+      serve = require('serve-handler'),
+      port = process.env.PORT || 3000
 
-function serve(res, ...paths) {
-    res.sendFile(resolve(__dirname, ...paths))
+const opts = {
+    public: resolve(__dirname, 'dist'),
+    directoryListing: false
 }
 
-function serveAssets(...paths) {
-    return express.static(resolve(__dirname, ...paths), { redirect: false })
-}
+const app = createServer((req, res) => serve(req, res, opts))
 
-app.use(serveAssets('dist'))
-app.use(serveAssets('static'))
-
-// there is a 404 page in `site.yml`, so
-// this handler must be placed before `/:page`
-app.get('/404', (req, res) => {
-    res.statusCode = 404
-    serve(res, 'dist', '404', 'index.html')
-})
-
-app.get('/:page', (req, res, next) => {
-    const { page } = req.params
-
-    if (page in pages)
-        serve(res, 'dist', page, 'index.html')
-    else
-        next()
-})
-
-app.all('*', (req, res) => res.redirect('/404'))
-
-app.listen(port, () => console.log('server is ready to accept connections on port ' + port))
+app.listen(port, () => console.log('server is ready to accept connections on port', port))
